8356fd08c24094c1f1bb9b5fd3825a48ad6e669c
Michael <mike_visualsoft@yahoo.com>
2015-03-24 15:47:51 +0700

    Part 1

---
+++ .tmp/public/scripts/register/1.js
@@ -0,0 +1,24 @@
+$(function(){
+      $('#proceed').click(function(e) {
+        //e.preventDefault();
+            var username = $('#username').val();
+            var password = $('#password').val();
+            var confirmation = $('#password2').val();
+            var ref="1";
+            if(username.length<=0)
+            {
+                  alert("Username harap diisi");
+                  return false;
+            }
+            if(password.length<6)
+            {
+                  alert("Password harus memiliki panjang minimal 6");
+                  return false;
+            }
+            if(password!=confirmation)
+            {
+                  alert("Password dan konfirmasi password harus sama");
+                  return false;
+            }
+      });
+});
